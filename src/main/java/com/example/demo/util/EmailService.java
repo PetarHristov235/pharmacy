b8ff44cf869e1ed22a63375434fcda8d536e23f2
unknown <petarhristov235@abv.bg>
2024-06-12 16:30:52 +0300

    rate us link port fixed

--- src/main/java/com/example/demo/util/EmailService.java
+++ src/main/java/com/example/demo/util/EmailService.java
@@ -11,7 +11,8 @@ import java.math.BigDecimal;
 public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
-    String link = "http://localhost:8080/rateUs";
+    private static final String RATE_US_URL = "http://localhost:8090/rateUs";
+
     public void orderConfirmationEmail(String receiverEmail,
                                        String receiverName,
                                        String orderedItems,
@@ -19,47 +20,46 @@ public class EmailService {
                                        String address,
                                        String phoneNumber,
                                        String date,
-                                       String orderNumber){
-        SimpleMailMessage message=new SimpleMailMessage();
+                                       String orderNumber) {
+        SimpleMailMessage message = new SimpleMailMessage();
 
         message.setFrom("blogemailservice512@gmail.com");
         message.setTo(receiverEmail);
-        message.setText("Скъпи "+receiverName+",\n" +
+        message.setText("Скъпи " + receiverName + ",\n" +
                 "\n" +
                 "Радваме се да ви информираме, че вашата поръчка беше успешно обработена и потвърдена.\n" +
                 "\n" +
                 "Детайлите на вашата поръчка:\n" +
                 "\n" +
-                "Номер на поръчка : "+orderNumber+"\n" +
-                "Дата на поръчка : "+date+"\n" +
-                "Адрес за доставка : "+address+"\n" +
-                "Телефонен номер : "+phoneNumber+"\n"+
+                "Номер на поръчка : " + orderNumber + "\n" +
+                "Дата на поръчка : " + date + "\n" +
+                "Адрес за доставка : " + address + "\n" +
+                "Телефонен номер : " + phoneNumber + "\n" +
                 "Поръчани продукти: \n"
-                +editItems(orderedItems)+
+                + editItems(orderedItems) +
                 "\n" +
-                "Обща сума : "+totalPrice+"\n" +
+                "Обща сума : " + totalPrice + "\n" +
                 "\n" +
                 "Моля, прегледайте горепосочените детайли и ни уведомете, ако имате въпроси или притеснения. Ако трябва да направите някакви промени в поръчката си, моля, свържете се с нас възможно най-скоро.\n" +
                 "\n" +
-                "Благодарим Ви, че ни се доверихте, за да се погрижим за вашето здраве!\n"+
-                "\n"+
-                "Ако сте доволни от нашето обслужване може да ни оцените като кликнете тук:\n"+
-                link+
-                "\n"+
-                "Поздрави,\n"+
+                "Благодарим Ви, че ни се доверихте, за да се погрижим за вашето здраве!\n" +
+                "\n" +
+                "Ако сте доволни от нашето обслужване може да ни оцените като кликнете тук:\n" +
+                RATE_US_URL +
+                "\n" +
+                "Поздрави,\n" +
                 "Екипът на Аптека Сияна"
         );
-        message.setSubject("Аптека Сияна - потвърждаване на поръчка: "+orderNumber);
+        message.setSubject("Аптека Сияна - потвърждаване на поръчка: " + orderNumber);
 
 
         mailSender.send(message);
     }
 
 
-
     public void registrationConfirmationEmail(String receiverEmail,
-                                          String receiverName
-                                      ) {
+                                              String receiverName
+    ) {
         SimpleMailMessage message = new SimpleMailMessage();
 
         message.setFrom("blogemailservice512@gmail.com");
@@ -68,9 +68,9 @@ public class EmailService {
                 "\n" +
                 "Радваме се да ви информираме, че вашата регистрация е направена успешно!\n" +
                 "\n" +
-                "Ще се радваме да помогнем за вашето здраве и красота!\n"+
+                "Ще се радваме да помогнем за вашето здраве и красота!\n" +
                 "\n" +
-                "Поздрави,\n"+
+                "Поздрави,\n" +
                 "Екипът на Аптека Сияна"
         );
         message.setSubject("Успешна регистрация");
@@ -78,8 +78,8 @@ public class EmailService {
         mailSender.send(message);
     }
 
-    private String editItems(String items){
-        return  items.replace("[", "").replace("]", "").replace(",", "");
+    private String editItems(String items) {
+        return items.replace("[", "").replace("]", "").replace(",", "");
     }
 
 }
